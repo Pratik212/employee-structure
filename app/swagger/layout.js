86a63cb9f01775e80630d77252893eec0ff0dfc8
Pratik212 <pratik.212.palanpure@gmail.com>
2025-07-06 18:27:45 +0530

    Remove un-used code.

--- app/swagger/layout.js
+++
@@ -1,7 +0,0 @@
-export default function SwaggerLayout({ children }) {
-    return (
-        <div style={{ backgroundColor: 'white', minHeight: '100vh' }}>
-            {children}
-        </div>
-    );
-}
